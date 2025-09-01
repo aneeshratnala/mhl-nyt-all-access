1cd4acfc914ed516851e50bde15600ae8cd4f39b
Aneesh Ratnala <aneeshratnala@ucla.edu>
2025-09-01 19:30:49 -0400

    added more nyt login page code

--- mhl-nyt-all-access.py
+++ mhl-nyt-all-access.py
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # details
 
-LIB_CARD_NUM = "21330001217930" # dummy number
+LIB_CARD_NUM = "21331112223333" # dummy number
 EMAIL = "aneeshratnala@ucla.edu"
 PW = "Ucla2024!" # dummy pw
 LIB_URL = "https://mhl.org/connect/20528"
@@ -27,7 +27,7 @@ try:
     driver.get(LIB_URL)
     print("Navigating to MHL URL...")
 
-    # 2. locate card num input box
+    # 2. locate card num input box, enter card num
     card_input_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "card-number")))
     
     card_input_box.send_keys(LIB_CARD_NUM)
@@ -50,13 +50,45 @@ try:
     redeem_button.click()
     print("Clicked redeem button.")
 
+    time.sleep(3) # wait for next page load
+    # if it says "We've sent a confirmation email to" then user is already signed in, all set
+
+    print(driver.page_source)
+
+    if "We've sent a confirmation email to" in driver.page_source:
+        print("Already signed in, all set.")
+        driver.quit()
+        exit(0)
+    elif "captcha-delivery.com" in driver.page_source:
+        input("Please solve the puzzle in the browser and then press Enter here to continue...")
+        print("Resuming script...")
+    
+
     # 6. locate email input box and fill
     email_input_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email")))
+    email_input_box.send_keys(EMAIL)
+
+    # 7. find continue and hit it, so PW box appears
+    continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@data-testid='submit-email']")))
+    continue_button.click()
+    print("Entered email and clicked continue.")
+
+    # 8. if a human verification pops up, wait for user to solve it (have to deal with this)
+    if "captcha-delivery.com" in driver.page_source:
+        input("Please solve the puzzle in the browser and then press Enter here to continue...")
+        print("Resuming script...")
+
+    # 9. find PW box and enter PW
+    pw_input_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
+    pw_input_box.send_keys(PW)
+
+    # 10. check for verification
+    time.sleep(3)
 
 except Exception as e:
     print(f"An error occurred: {e}")
-    driver.quit()
-    exit(1)
+    # driver.quit()
+    # exit(1)
     
 finally:
     # driver.quit()
